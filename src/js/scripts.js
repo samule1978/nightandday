71914fc3058175b07508c815252648dd57bdc55b
Samule Ghurry <samule.ghurry@coxauto.co.uk>
2022-06-12 13:24:15 +0100

    Ver 1.0.0.1: Setup - GSAP.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -11,7 +11,6 @@ initSiteConfig();
 sleep(3000).then(function () {
 	//makeToast("7 People are viewing this vehicle right now.", "top-right", true, false);
 	//makeToast("7 People are viewing this vehicle right now.");
-
 	/*sleep(3000).then(function () {
 		makeToast("3 People have enquired about this vehicle.");
 
@@ -26,7 +25,7 @@ sleep(3000).then(function () {
 if (!isTouchEnabled()) {
 	if (is(VanillaTilt)) {
 		VanillaTilt.init(qsa(".component--featured-card[sg78-fx-3d]"), {
-		//VanillaTilt.init(qsa(".component--featured-card"), {
+			//VanillaTilt.init(qsa(".component--featured-card"), {
 			scale: 1.07,
 			perspective: 2000,
 			max: 15,
@@ -42,41 +41,29 @@ qsa("[sg78-ux-toastie]")[0].addEventListener("click", (e) => {
 	}
 });
 
-if (is(qs("[sg78-ux-scroll-tracker]"))) {
-	document.body.prepend(
-		createElement("div", {
-			classList: "scroll-tracker",			
-		})
-	);
+if (is(qs("[sg78-ux-scroll-trigger]"))) {
+	gsap.registerPlugin(ScrollTrigger);
+
+	let progress = createElement("progress");
+	progress.setAttribute("max", "100");
+	progress.setAttribute("value", "0");
+	document.body.prepend(progress);
 
 	qs(["header"]).prepend(
 		createElement("div", {
-			classList: "bg-day",			
+			classList: "bg-day",
 		})
 	);
 
-
-
-	import('./vendor/scroll-timeline/scroll-timeline.js').then(() => {
-		const bgDay = qs(".bg-day");
-		const scrollTracker = qs(".scroll-tracker");
-		const scrollTrackingTimeline = new ScrollTimeline({
-			source: document.scrollingElement,
-			orientation: "block",
-			scrollOffsets: [CSS.percent(0), CSS.percent(100)],
-		});
-
-		scrollTracker.animate(
-			{ transform: ["scaleX(0)", "scaleX(1)"] },
-			{ timeline: scrollTrackingTimeline }
-		);
-		
-
-		
-
-		bgDay.animate(
-			{ opacity: ["0", "1"] },
-			{ timeline: scrollTrackingTimeline }
-		);
+	gsap.to("progress", {
+		value: 100,
+		ease: "none",
+		scrollTrigger: { scrub: 0.3 },
 	});
-}
+
+	gsap.to(".bg-day", {
+		opacity: 0,
+		ease: "none",
+		scrollTrigger: { scrub: 0.3 },
+	});
+};
