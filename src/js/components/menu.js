a787d11685e4c4b355d6852b55b6063a540ff028
Samule Ghurry <samule.ghurry@coxauto.co.uk>
2022-07-03 14:52:03 +0100

    Ver 1.0.1.5: Logo.

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -43,21 +43,18 @@ export class Menu {
 	};
 
     moveLogo() {
-        let moveRight = false;
-        let moveUp = false;
+        const threshold = 50;
+        let moveRight, moveUp = false;
         let diffX = 0, diffY = 0;
+
         if (this.input.x.end < this.input.x.start) {
-            //console.log(`Left: X start = ${this.input.x.start}, X end = ${this.input.x.end}`);
             moveRight = false;
-            diffX = this.input.x.start - this.input.x.end;
-            
+            diffX = this.input.x.start - this.input.x.end;            
         }
         if (this.input.x.end > this.input.x.start) {
-            //console.log(`Right: X start = ${this.input.x.start}, X end = ${this.input.x.end}`);
             moveRight = true;
             diffX = this.input.x.end - this.input.x.start;
         }
-
         if (this.input.y.end < this.input.y.start) {
             moveUp = true;
             diffY = this.input.y.start - this.input.y.end;            
@@ -67,25 +64,25 @@ export class Menu {
             diffY = this.input.y.end - this.input.y.start;            
         }
 
-        console.log(`diffX: ${diffX}, diffY = ${diffY}`);
-
         if (diffX >= diffY) {
-            // Moving on x axis.
-            if (moveRight) {
-                this.moveLogoRight();
-            } else {
-                this.moveLogoLeft();
+            if (diffX > threshold) {
+                // Moving on x axis.
+                if (moveRight) {
+                    this.moveLogoRight();
+                } else {
+                    this.moveLogoLeft();
+                }
             }
         } else {
-            // Moving on y axis.
-            if (moveUp) {
-                this.moveLogoUp();
-            } else {
-                this.moveLogoDown();
+            if (diffY > threshold) {
+                // Moving on y axis.
+                if (moveUp) {
+                    this.moveLogoUp();
+                } else {
+                    this.moveLogoDown();
+                }
             }
         }
-
-
     }
     moveLogoRight() {
         if (this.menu.getAttribute("sg78-logo-menu-x") == "left") {
@@ -119,43 +116,16 @@ export class Menu {
 
     createTimeLines() {
 		this.tlLeftToRight.pause();
-		//this.tlLeftToRight.add(gsap.to(this.menu, { rotation: -20, ease: "back.out", duration: 0.25 }));
-		//this.tlLeftToRight.add(gsap.to(this.menu, { rotation: 20, ease: "back.in", duration: 0.25 }));
 		this.tlLeftToRight.add(gsap.to(this.menu, { rotation: 0, ease: "bounce.out", left: "100%", x: `-${this.offset.x.end}}`, duration: 1 }));
-        //this.tlLeftToRight.add(gsap.to(this.menu, { rotation: 0, ease: "back.in",  duration: 0.25 }));
-
 
 		this.tlRightToLeft.pause();
-		//this.tlRightToLeft.add(gsap.to(this.menu, { rotation: 20, ease: "back.out", duration: 0.25 }));
-		//this.tlRightToLeft.add(gsap.to(this.menu, { rotation: -20, ease: "back.in", duration: 0.25 }));
 		this.tlRightToLeft.add(gsap.to(this.menu, { rotation: 0, ease: "bounce.out", left: "0%", x: `${this.offset.x.start}}`, duration: 1 }));
-        //this.tlRightToLeft.add(gsap.to(this.menu, { rotation: 0, ease: "back.in",  duration: 0.25 }));
-
-
 
 		this.tlTopToBottom.pause();
 		this.tlTopToBottom.add(gsap.to(this.menu, { rotation: 0, ease: "bounce.out", top: "100%", y: `-${this.offset.y.end}}`, duration: 1 }));
 
 		this.tlBottomToTop.pause();
 		this.tlBottomToTop.add(gsap.to(this.menu, { rotation: 0, ease: "bounce.out", top: "0%", y: `${this.offset.y.start}}`, duration: 1 }));
-    }
-
-    updateLogoPosition() {        
-        if (this.menu.getAttribute("sg78-logo-menu-x") == "") {
-            this.menu.setAttribute("sg78-logo-menu-x", "left");
-        } else if (this.menu.getAttribute("sg78-logo-menu-x") == "left") {
-            this.menu.setAttribute("sg78-logo-menu-x", "right");
-        } else if (this.menu.getAttribute("sg78-logo-menu-x") == "right") {
-            this.menu.setAttribute("sg78-logo-menu-x", "left");
-        }
-        
-        if (this.menu.getAttribute("sg78-logo-menu-y") == "") {
-            this.menu.setAttribute("sg78-logo-menu-y", "top");
-        } else if (this.menu.getAttribute("sg78-logo-menu-y") == "top") {
-            this.menu.setAttribute("sg78-logo-menu-y", "bottom");
-        } else if (this.menu.getAttribute("sg78-logo-menu-y") == "bottom") {
-            this.menu.setAttribute("sg78-logo-menu-y", "top");
-        }                
     }
 
     init() {        
